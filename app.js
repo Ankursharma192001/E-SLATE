407cf4ef9dcbad581cf0feabfe3363be3c3b37a1
ankursharma192001 <as0024364@gmail.com>
2022-11-02 20:48:03 +0530

    move to public

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const socket = require("socket.io");
 
 const app = express(); //Initialized and server ready
 
-app.use(express.static("OPEN BOARD"));
+app.use(express.static("public"));
 
 let port = process.env.PORT || 5000;
 let server = app.listen(port, () => {
@@ -14,4 +14,16 @@ let io = socket(server);
 
 io.on("connection", (socket) => {
     console.log("Made socket connection");
+    // Received data
+    socket.on("beginPath", (data) => {
+        // data -> data from frontend
+        // Now transfer data to all connected computers
+        io.sockets.emit("beginPath", data);
+    })
+    socket.on("drawStroke", (data) => {
+        io.sockets.emit("drawStroke", data);
+    })
+    socket.on("redoUndo", (data) => {
+        io.sockets.emit("redoUndo", data);
+    })
 })
